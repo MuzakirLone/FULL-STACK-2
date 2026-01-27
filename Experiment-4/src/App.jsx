22723cd509d4a0c87d08971353491f995619a413
MuzakirLone <muzakirmasood2005@gmail.com>
2026-01-27 14:33:07 +0530

    Fix numbering in Local State counters

--- Experiment-4/src/App.jsx
+++ Experiment-4/src/App.jsx
@@ -9,16 +9,7 @@ function App() {
     <CounterProvider>
       <div className="app-container">
         {/* Local State Section */}
-        <CounterLocal cno="2" />
-        {/* <CounterLocal cno="2" /> Remove second if only one shown in image, but user said 6 sections. 
-            User image shows: 
-            2: Local State
-            1: Global (Context)
-            2: Global (Context)
-            1: Global (Redux)
-            2: Global (Redux) 
-            Total 5 visible in image? Request says "Make 6 sections... 2 Local...". I'll do 2 for local too. 
-        */}
+        <CounterLocal cno="1" />
         <CounterLocal cno="2" />
 
         {/* Global State (ContextAPI) Section */}
